a32366ebf37367248dc98ac90bae66d712e03a81
pedroramos17 <pedro_ramos2003@hotmail.com>
2022-08-29 20:04:47 -0300

    soma

--- calcula.php
+++ calcula.php
@@ -1,12 +1,19 @@
-$num1
-$num2
-$result
-$operacao
-$operador
+<?php
+$num1 = 10;
+$num2 = 20;
+$result = 0;
+$operacao = "";
+$operador = 1;
 
 switch($operador){
     case 1 :
-        echo "O resultado da ".$operacao." é ".somaValor(num1, num2);
+        $operacao = "soma";
+        echo "O resultado da ".$operacao." é ".somaValor($num1, $num2).".";
         break;
-    default : echo "Resultado inválido!"
+    default : echo "Resultado inválido!";
 }
+
+function somaValor($num1, $num2){
+    return($num1+$num2);
+}
+?>
