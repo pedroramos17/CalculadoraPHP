689b811565461d7c06d938a59ef7f2b92cf2f7c3
pedroramos17 <pedro_ramos2003@hotmail.com>
2022-08-29 19:48:08 -0300

    first-commit

---
+++ calcula.php
@@ -0,0 +1,12 @@
+$num1
+$num2
+$result
+$operacao
+$operador
+
+switch($operador){
+    case 1 :
+        echo "O resultado da ".$operacao." é ".somaValor(num1, num2);
+        break;
+    default : echo "Resultado inválido!"
+}
